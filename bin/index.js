4a05b28ecaa61a120550f68dfb9bec66071d6ab1
heyolaniran <olaniran.abd@gmail.com>
2023-07-14 12:50:17 +0100

    feat: setting mode and path argvs for cli

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,38 @@
 #! /usr/bin/env node
+const yargs = require("yargs/yargs")
+const availableMode = ['build' , 'start'] ; 
+yargs(process.argv.slice(2))
+.options('mode' , {
+    alias : 'm' , 
+    describe : "Would you 'build' your project for production or just 'start' your servers ?"
+})
+.option('path', {
+    alias: 'p',
+    describe: 'Where is located your amazing project on your Disk ? '
+  })
+  .demandOption(['mode', ], 'Please provide mode argument to launch your project with this tool')
+  .help()
+  .argv
 
-const argv = require("yargs/yargs")(process.argv).argv ; 
+const argv = yargs(process.argv).argv ; 
 
-console.log(argv.m) ; 
+const defaultMode = "build" ; 
+var mode = argv.m || argv.mode   ; 
+var path = argv.p  || argv.path ; 
+
+if(path) { 
+    console.log("path to apply" , path); 
+}else 
+{
+    console.log("default path is ."); 
+}
+
+if(mode !== undefined) { 
+    if(availableMode.includes(mode)) {
+        console.log(' Your current valid  mode is  : ',  mode) ; 
+    }else {
+        mode = defaultMode ; 
+        console.log(' Your default valid  mode is  : ',  mode) ; 
+    }
+}
+
